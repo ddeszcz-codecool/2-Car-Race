cd0859af0ce1d4edf056ea46043d221dc97907cf
Dawid Deszcz <d.deszcz@gmail.com>
2022-10-04 13:40:42 +0200

    Implement Weather class

--- src/main/java/com/codecool/car_race/Weather.java
+++ src/main/java/com/codecool/car_race/Weather.java
@@ -1,4 +1,23 @@
 package main.java.com.codecool.car_race;
 
+import main.java.com.codecool.car_race.util.RandomHelper;
+
 public class Weather {
+    private static final int CHANCE_OF_RAIN = 30;
+    private boolean isRaining = false;
+
+    public Weather() {
+        randomize();
+    }
+
+    public boolean isRaining() {
+        return isRaining;
+    }
+
+
+    public void randomize() {
+        isRaining = RandomHelper.eventWithChance(CHANCE_OF_RAIN);
+    }
+
+
 }
